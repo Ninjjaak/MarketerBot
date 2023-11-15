4fb755f9f75f65d3519218ef53c0c656ad858f2d
Ninjjaak <134675690+Ninjjaak@users.noreply.github.com>
2023-11-15 22:31:04 +0800

    Update linkedin.py

--- linkedin.py
+++ linkedin.py
@@ -2,24 +2,23 @@ import requests
 from notion_client import Client as NotionClient
 import openai
 
-# Function to fetch data from Notion
+
 def fetch_data_from_notion(notion_token, page_id):
     notion = NotionClient(auth=notion_token)
     page_content = notion.pages.retrieve(page_id=page_id)
     
-    # Process page_content to extract the text you want to summarize
-    extracted_text = page_content.get('property_name')  # Replace 'property_name' with the actual property name containing text
+
+    extracted_text = page_content.get('property_name')  
     
     return extracted_text
 
-# Function to summarize the content using OpenAI's model
+
 def get_summary_from_openai(openai_api_key, text_to_summarize):
     openai.api_key = openai_api_key
     response = openai.Completion.create(engine="davinci", prompt=text_to_summarize, max_tokens=150)
     summary = response.choices[0].text.strip()
     return summary
 
-# Function to post content to LinkedIn
 def post_to_linkedin(linkedin_access_token, summary):
     url = 'https://api.linkedin.com/v2/ugcPosts'
     headers = {
@@ -27,15 +26,22 @@ def post_to_linkedin(linkedin_access_token, summary):
         'X-Restli-Protocol-Version': '2.0.0',
         'Content-Type': 'application/json',
     }
-    # Construct the post body according to LinkedIn API specifications
+    
     post_data = {
-        # ... LinkedIn post data format ...
+        "author": "urn:li:person:352936299", 
+        "lifecycleState": "PUBLISHED",
+        "specificContent": {
+            "com.linkedin.ugc.ShareContent": {
+                "shareCommentary": {
+                    "text": summary
+                },
+                "shareMediaCategory": "NONE",
+            }
+        },
+        "visibility": {
+            "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
+        }
     }
-    response = requests.post(url, headers=headers, json=post_data)
-    if response.status_code == 201:
-        print("Successfully posted to LinkedIn")
-    else:
-        print("Failed to post to LinkedIn:", response.content)
 
 # Main workflow
 def main(notion_token, page_id, openai_api_key, linkedin_access_token):
